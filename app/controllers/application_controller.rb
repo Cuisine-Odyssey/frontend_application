2f3e749b3a9bd245d010f360c712f480013aab0a
Dylan Harper <dylanharper4@icloud.com>
2022-02-24 20:27:00 -0800

    Add if current_user logic to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  # helper_method :current_user
+  helper_method :current_user
 
   # def current_user
   #   User.find(session[:user_id]) if session[:user_id]
   # end
 
+  def current_user#ask erin
+    if session[:user_id]#can we send lots of user data with sessions
+      User.new(session_data)
+    end
+  end
   # def require_user
   #   if !current_user
   #     flash[:alert] = 'Login Required!'

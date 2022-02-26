fcc0bdfb6c35c6a040cfaa378a4ca9fc07767594
Dylan Harper <dylanharper4@icloud.com>
2022-02-26 15:29:53 -0800

    Get FE facade to talk to BE DB

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,8 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-    email = auth_hash['info']['email']
-
-    binding.pry
-    @user = UserFacade.find_or_create_user(email: email)
+    @user = UserFacade.find_or_create_user(auth_hash)
+    require "pry"; binding.pry
     session[:access_token] = auth_hash['credentials']['token']
     redirect_to root_path
   end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,15 +1,14 @@
 class UserService
   def self.connection
-    Faraday.new(url: 'https://localhost:3000/api/v1/') do |faraday|
-      # faraday.params['api_key'] = ENV['backend_api_key']
+    Faraday.new(url: 'http://localhost:3000/api/v1/') do |faraday|
     end
   end
 
-  def self.get_user_by_email(email)
-    response = connection.get("users?email='#{email}") do |request|
-      # request.params['api_key'] = ENV['backend_api_key']
+  def self.get_user(params)
+    response = connection.post("users") do |request|
+      request.headers['Content-Type'] = 'application/json'
+      request.body = JSON.generate(user: params)
     end
-
     JSON.parse(response.body, symbolize_names: true)
   end
 

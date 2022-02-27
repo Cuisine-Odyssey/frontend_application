1b8402406a00722091b3b273a336ead51bf275db
Kelly Anderson <kean9528@colorado.edu>
2022-02-27 15:42:43 -0700

    get VCR to work

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,23 +1,19 @@
 class UserService
-<<<<<<< HEAD
 
-=======
->>>>>>> deb68be2ae2bb8fa27c487e392d1068e3153ff63
   def self.connection
     Faraday.new(url: 'http://localhost:3000/api/v1/') do |faraday|
     end
   end
 
-<<<<<<< HEAD
   def self.get_user_by_email(email)
-    response = connection.get("users?email=#{email}") do |request|
-      # request.params['api_key'] = ENV['backend_api_key']
-=======
+    response = connection.get("users?email=#{email}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.get_user(params)
     response = connection.post("users") do |request|
       request.headers['Content-Type'] = 'application/json'
       request.body = JSON.generate(user: params)
->>>>>>> deb68be2ae2bb8fa27c487e392d1068e3153ff63
     end
     JSON.parse(response.body, symbolize_names: true)
   end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -2,20 +2,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipe Service' do
-  it 'returns a Faraday response' do
-    expect(RecipeService.connection).to be_a(Faraday::Connection)
+  it 'returns a faraday response', :vcr do
+    VCR.use_cassette('returns_a_faraday_response') do
+      connection = RecipeService.connection
+      expect(connection).to be_a(Faraday::Connection)
+    end
   end
 
-  it 'returns a Recipe at random' do
-    random_recipe = RecipeService.get_random_recipe
-    expect(random_recipe).to be_a Hash
-    expect(random_recipe[:meals]).to be_a Array
-    expect(random_recipe[:meals][0]).to have_key :idMeal
+  it 'returns a recipe at random', :vcr do
+    VCR.use_cassette('returns_a_recipe_at_random') do
+      random_recipe = RecipeService.get_random_recipe
+      expect(random_recipe).to be_a Hash
+      expect(random_recipe[:meals]).to be_a Array
+      expect(random_recipe[:meals][0]).to have_key :idMeal
+    end
   end
 
-  it 'returns a list of up to 5 recipes from random regions' do
-    recipe_list = RecipeService.get_five_recipes
-    expect(recipe_list).to be_a Array
-    expect(recipe_list.count).to eq(5)
+  it 'returns a list of up to 5 recipes from random regions', :vcr do
+    VCR.use_cassette('returns_a_list_of_up_to_5_recipes_from_random_regions') do
+      recipe_list = RecipeService.get_five_recipes
+      expect(recipe_list).to be_a Array
+      expect(recipe_list.count).to eq(5)
+    end 
   end
 end

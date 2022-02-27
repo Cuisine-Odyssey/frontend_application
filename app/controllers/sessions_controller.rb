cbb3126a38c5f2db5d47b897ee1dfa111bf3daad
Dylan Harper <dylanharper4@icloud.com>
2022-02-26 17:48:18 -0800

    Add sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     @user = UserFacade.find_or_create_user(auth_hash)
-    # if @user
-      session[:access_token] = auth_hash['credentials']['token']
-      # end
-require "pry"; binding.pry
+    session[:access_token] = auth_hash['credentials']['token']
+    redirect_to root_path
+  end
+
+  def destroy
+    session.destroy
     redirect_to root_path
   end
 end

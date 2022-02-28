577e6a3248ccf2bda56796094d1626e0d16d1dfd
David Kassin <david.j.kassin@gmail.com>
2022-02-28 09:57:16 -0700

    Add recipe location and logic for unregistered user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    one_recipe = RecipeFacade.get_random_recipe
-    @five_recipes = RecipeFacade.get_five_recipes(one_recipe.location)
+    @one_recipe = RecipeFacade.get_random_recipe
+    @five_recipes = RecipeFacade.get_five_recipes(@one_recipe.location)
   end
 end
--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Recipe Index Page' do
   describe 'view' do
-    xit 'shows recipes on the recipe page', :vcr do
+    it 'shows recipes on the recipe page', :vcr do
       visit recipes_path
 
+      within "#location" do
+        expect(page).to have_content("Canadian")
+      end
+      save_and_open_page
       click_link('Recipe')
     end
   end

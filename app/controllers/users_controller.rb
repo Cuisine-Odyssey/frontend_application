eac869d7f748479357c4003c0dbe594b6f345047
Dylan Harper <dylanharper4@icloud.com>
2022-02-28 18:29:29 -0800

    Edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    hash =
-    # { 'info' =>
-      {
+    hash =  {
       'email' => session['email'],
       'first_name' => session['first_name'],
       'last_name' => session['last_name']
       }
-    # }
 
-    # @user = UserFacade.find_or_create_user(hash)
     @recipes = UserFacade.get_user_recipes(hash)
     require "pry"; binding.pry
   end

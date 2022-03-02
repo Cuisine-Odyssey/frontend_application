a24ba975c2ecb51d404f278cb873595046038ac4
David Kassin <david.j.kassin@gmail.com>
2022-03-02 11:16:59 -0700

    Add css tests for cocktails index view

--- spec/features/cocktails/index_spec.rb
+++ spec/features/cocktails/index_spec.rb
@@ -3,7 +3,9 @@ RSpec.describe 'Cocktails show page' do
   it 'can view cocktail details' do
     visit '/cocktails'
 
-    save_and_open_page
+    expect(page).to have_css("#cocktail-name")
+    expect(page).to have_css("#cocktail-instructions")
+    expect(page).to have_css("#cocktail-ingredients")
     expect(page).to have_css("img")
   end
 end

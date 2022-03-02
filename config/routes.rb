62a987e0417afb8fd197b156222290ccadb27985
David Kassin <david.j.kassin@gmail.com>
2022-03-02 10:52:32 -0700

    Add routes for cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'users#show'
 
   resources :recipes, only: [:index, :show]
-
+  resources :cocktails, only: [:index]
   post '/recipes/:id/like', to: 'recipes#like'
   post '/recipes/:id/dislike', to: 'recipes#dislike'
 end

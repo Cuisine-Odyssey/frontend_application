e2b9df95e88c9fbfc95c1bd3102dec965919144e
Hannah Warren <hannahkwarren@gmail.com>
2022-03-03 12:11:45 -0500

    Cocktail service tests

--- spec/services/cocktail_service_spec.rb
+++ spec/services/cocktail_service_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe 'Cocktail Service' do
     expect(random_cocktail[:drinks][0]).to have_key :strMeasure1
   end
 
+  it 'returns a faraday response', :vcr do
+    intl_connection = CocktailService.internal_connection_cocktail
+    expect(intl_connection).to be_a(Faraday::Connection)
+  end
+
   it 'returns a specified cocktail', :vcr do
     specific_cocktail = CocktailService.get_cocktail_data(11007)
 
@@ -26,5 +31,16 @@ RSpec.describe 'Cocktail Service' do
     expect(specific_cocktail[:drinks][0]).to have_key :strInstructions
     expect(specific_cocktail[:drinks][0]).to have_key :strIngredient1
     expect(specific_cocktail[:drinks][0]).to have_key :strMeasure1
+  end
+
+  xit 'sends a cocktail vote', :vcr do
+    VCR.use_cassette('gets_successful_response') do
+      custom_params = {
+        'recipe_api_id': 52896,
+        'email': params[:email],
+        'vote': 'like'
+      }
+      vote = CocktailService.send_cocktail_vote(custom_params)
+    end
   end
  end

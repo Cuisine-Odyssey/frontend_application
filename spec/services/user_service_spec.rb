5e6ac520fb340c63bf38a8d07228f94bc1add18d
Hannah Warren <hannahkwarren@gmail.com>
2022-03-03 13:11:19 -0500

    User service test

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe 'User Service' do
 
     expect(service_returns.email).to eq('wookie@gmail.com')
   end
+
+  it 'creates or updates user' do
+    user = instance_double('user', first_name: 'Chewie', last_name: 'Bacca', email: 'wookie@gmail.com')
+    allow(UserService).to receive(:create_or_update_user).and_return(user)
+
+    service_returns = UserService.create_or_update_user(user)
+
+    expect(service_returns.first_name).to eq('Chewie')
+    expect(service_returns.last_name).to eq('Bacca')
+    expect(service_returns.email).to eq('wookie@gmail.com')
+  end
 end
